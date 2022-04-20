4101eb530efee5cbbe8d7b0d5cf3bcc7a7d7e3ca
brandonruizmora <brandon_ruizmor@outlook.es>
2022-04-19 22:11:19 -0500

    added test suit for MissionCommander class

--- test/missionCommander.test.js
+++ test/missionCommander.test.js
@@ -1,6 +1,16 @@
+const MissionCommander = require('./../app/missionCommander');
+
 describe("Esto es una suite de pruebas", () => {
     test('Caso de prueba 1', () => {
         const result = 1 + 2
         expect(result).toBe(3);
+    });
+})
+
+describe('Pruebas en clase MissionCommander', () => {
+    test('1.- Crear un objeto de la clase', () => {
+        const myMissionCommander = new MissionCommander('Woopa');
+        expect(myMissionCommander.name).toBe('Woopa');
+        expect(myMissionCommander).toEqual({name: 'Woopa'});
     });
 })
